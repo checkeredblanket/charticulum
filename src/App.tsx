f1305d139d69f72d87a99b9c582b87a55106907c
checkb <thecheckeredblanket@proton.me>
2026-09-21 16:04:44 -0400

    update landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+import { useState } from 'react';
+
 function App() {
+  const [showModal, setShowModal] = useState(false)
   return (
     <div className="app">
       <header>Charticulum</header>
       <main>
         <h1>Chart Your Curriculum</h1>
         <p>The Visual Academic Planner</p>
+        <button onClick={() => setShowModal(!showModal)}>
+          {showModal ? 'Close' : 'Get Started'}
+        </button>
       </main>
-      <footer>@ 2026 Charticulum</footer>
+      <footer>@ 2026 Charticulum. All rights reserved.</footer>
     </div>
   )
 }
